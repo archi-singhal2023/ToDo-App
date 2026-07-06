df7d20f71e3e04d37b1a7ddea0030d46d614cb48
Archi <archi.singhal2023@ssipmt.com>
2026-07-06 18:35:06 +0530

    Initialized app using Flask and SQLAlchemy

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+#create database object globally
+db = SQLAlchemy()
+
+def create_app():
+    app = Flask(__name__)
+    
+    app.config['SECRET KEY'] = 'damnn-a-to-do'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+    
+    db.__init__(app)
+
+    from app.routes.auth import auth_bp
+    from app.routes.auth import tasks_bp
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(tasks_bp)
+    
+    return app
+    
