6637bdefb23bb204ecc7eb785bf9cb425ca2acee
Archi <archi.singhal2023@ssipmt.com>
2026-07-07 23:26:20 +0530

    Built the main file and debugged the errors

--- app/__init__.py
+++ app/__init__.py
@@ -7,14 +7,14 @@ db = SQLAlchemy()
 def create_app():
     app = Flask(__name__)
     
-    app.config['SECRET KEY'] = 'damnn-a-to-do'
+    app.config['SECRET_KEY'] = 'a-to-do'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
     
-    db.__init__(app)
+    db.init_app(app)
 
     from app.routes.auth import auth_bp
-    from app.routes.auth import tasks_bp
+    from app.routes.tasks import tasks_bp
     app.register_blueprint(auth_bp)
     app.register_blueprint(tasks_bp)
     
